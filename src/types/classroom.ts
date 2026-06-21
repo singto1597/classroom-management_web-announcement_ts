509ba4549334ba2c72839879d1d58b6e1ad887ad
singto1597 <singto1597@gmail.com>
2026-06-21 23:03:23 +0700

    Fix: edit ui

--- src/types/classroom.ts
+++ src/types/classroom.ts
@@ -1,8 +1,9 @@
 export interface UserRoom {
-  room_id: number; 
+  room_id: number;
   server_id?: string;
-  server_id_str?: string; 
+  server_id_str?: string;
   room_code?: string;
   room_name: string;
   role: string;
+  status: string; // ✨ เพิ่มบรรทัดนี้เข้าไปแค่นี้เลยครับ!
 }
